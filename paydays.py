0757f0614205c1b4738790e5360b833d056599a0
Alex McBride <xandermcbride@gmail.com>
2020-03-01 12:25:56 +0000

    Updated holidays

--- paydays.py
+++ paydays.py
@@ -3,15 +3,10 @@ import datecalc
 
 HOLIDAYS = [
     datetime(2020, 1, 1),
-    datetime(2020, 3, 17),
     datetime(2020, 4, 10),
     datetime(2020, 4, 13),
-    datetime(2020, 5, 4),
     datetime(2020, 5, 8),
     datetime(2020, 5, 25),
-    datetime(2020, 6, 1),
-    datetime(2020, 6, 13),
-    datetime(2020, 8, 3),
     datetime(2020, 8, 31),
     datetime(2020, 12, 25),
     datetime(2020, 12, 18)
@@ -20,3 +15,4 @@ HOLIDAYS = [
 print 'Paydays:'
 for payday in datecalc.getDays(26, HOLIDAYS):
     print payday.strftime('%a %d %b %Y')
+    
